ea243dbd908b8c8319f1a6c19327870af4ce331b
Davon <tie-n-dev@ntymail.com>
2018-10-04 18:21:49 +0200

    Create pad and move by mouse

--- main.lua
+++ main.lua
@@ -7,6 +7,12 @@ love.graphics.setDefaultFilter("nearest")
 -- Déboguage pas à pas dans la console
 if arg[#arg] == "-debug" then require("mobdebug").start() end
 
+pad = {
+  x = 100,
+  y = 20,
+  posX = 0,
+  posY = 0
+  }
 
 -- Charger une fois au démarrage
 function love.load()
@@ -14,14 +20,16 @@ function love.load()
   largeur = love.graphics.getWidth()
   hauteur = love.graphics.getHeight()
   
+  posY = hauteur - pad.y
+  
 end
 
 -- Recharger 60 fois par seconde (ou autant que faire se peut)
 function love.update(dt)
-  
+  posX = love.mouse.getX() - pad.x / 2
 end
 
 -- Affichage à l'écran
 function love.draw()
-  
+  love.graphics.rectangle("fill", posX, posY, pad.x, pad.y)
 end
